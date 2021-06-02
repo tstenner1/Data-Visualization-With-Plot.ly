e6a452ebfe0e3a05801281d4a37e71d7cc611f24
Thomas Stenner <thomasstenner@thomass-mbp.attlocal.net>
2021-06-02 16:18:39 -0500

    new

--- app.js
+++ app.js
@@ -5,3 +5,11 @@ function buildMetadata(selection) {
     d3.json("samples.json").then((sampleData) => {
 
         console.log(sampleData);
+    // Parse and filter the data for sample metadata
+        var parsedData = sampleData.metadata;
+        console.log("parsed data inside buildMetadata function")
+        console.log(parsedData);
+
+        var sample = parsedData.filter(item => item.id == selection);
+        console.log("showing sample[0]:");
+        console.log(sample[0]);
